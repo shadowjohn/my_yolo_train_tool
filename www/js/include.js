54f7307d3ba4b57db6c9b83c6c1f8cf32c0ee540
John <john@gis.tw>
2025-07-14 18:05:40 +0800

    開始訓練的功能

--- www/js/include.js
+++ www/js/include.js
@@ -274,3 +274,168 @@ function arduino_map(x, in_min, in_max, out_min, out_max) {
     //out 如 0~1024
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+function myAjax(url, postdata) {
+    var $method = "POST";
+    if (postdata == null || postdata == "") {
+        $method = "GET";
+    }
+    var tmp = $.ajax({
+        url: url,
+        type: $method,
+        data: postdata,
+        async: false
+    }).responseText;
+    return tmp;
+}
+function getWindowSize() {
+    var myWidth = 0, myHeight = 0;
+    if (typeof (window.innerWidth) == 'number') {
+        //Non-IE
+        myWidth = window.innerWidth;
+        myHeight = window.innerHeight;
+    } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+        //IE 6+ in 'standards compliant mode'
+        myWidth = document.documentElement.clientWidth;
+        myHeight = document.documentElement.clientHeight;
+    } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
+        //IE 4 compatible
+        myWidth = document.body.clientWidth;
+        myHeight = document.body.clientHeight;
+    }
+    var a = new Object();
+    a['width'] = myWidth;
+    a['height'] = myHeight;
+    return a;
+}
+function selectToCheckbox(select_jQDom, options) {
+    //用途，把 select 變成 checkbox，當 checkbox 選擇後，等同 select 被切換，表單只需回傳 select 的資料即可，省得寫一堆 checkbox
+    //注：my_fix_random 在 myphp.js 裡面，符合資安等級亂數
+    //注：原 select 需先加入「multiple="multiple"」，才不會 init 後預選第一筆
+    //Version: 0.02
+    //Author: 羽山秋人 (https://3wa.tw)
+    //
+    //return object
+    //{ jQDom: 新的 checkbox jquery doms ,html: checkbox_html 內容, reqk: 'selectToCheckbox_{timestame}' }
+    //options:
+    if (select_jQDom.attr('selectToCheckbox_reqk') != null) {
+        //已註冊過 checkbox ，全都移除
+        $("*[reqk='" + select_jQDom.attr('selectToCheckbox_reqk') + "']").remove();
+        select_jQDom.unbind(select_jQDom.data()["selectToCheckbox_event"]);
+    }
+    var reqk = 'selectToCheckbox_' + new Date().getTime() + '_' + my_fix_random();
+    //把 select 註冊 selectToCheckbox_reqk
+    select_jQDom.attr({
+        'selectToCheckbox_reqk': reqk,
+        'multiple': 'multiple'
+    });
+
+
+    var default_options = {
+        "separate": '&nbsp;', //分格符號，預設為空白
+        "output_format": "dom", // 可為 dom 或 html，如果單純為 html ，就沒有 jQDom 輸出，但會有註冊屬性的 funnction 回傳，需自行執行 (execute_event)
+        "checkboxhtml_to_dom": null, //如果這裡是空值，就會在原來的 select 後面接上
+        "class": null //css class，可自定名稱
+    };
+    if (options != null && typeof (options) == "object") {
+        for (var k in options) {
+            k = k.toLowerCase();
+            switch (k) {
+                case 'output_format':
+                    default_options[k] = options[k].toLowerCase();
+                    break;
+                default:
+                    default_options[k] = options[k];
+                    break;
+            }
+        }
+    }
+    var output = new Object();
+    output['jQDom'] = null;
+    output['html'] = null;
+    output['reqk'] = null;
+    output['execute_event'] = null;
+    if (select_jQDom.length == 0) {
+        console.log("Select 不存在...");
+        return output;
+    }
+    // select to array
+    var m = new Array();
+    var select_option_doms = select_jQDom.find("option");
+    for (var i = 0, max_i = select_option_doms.length; i < max_i; i++) {
+        var d = {
+            'value': select_option_doms.eq(i).val(),
+            'value_label': select_option_doms.eq(i).text()
+        };
+        m.push(d);
+    }
+    //註冊事件
+    output['execute_event'] = function () {
+        //當 checkbox 被按到的時候，原來的 select 會變化
+        $("input[type='checkbox'][reqk='" + reqk + "']").off().bind("click", {}, function (e) {
+            //收集有哪些 checkbox 現在是 checked 是 true，組合成 array
+            var checked_doms = $("input[type='checkbox'][reqk='" + reqk + "']:checked");
+            var m_true_values = new Array();
+            for (var i = 0, max_i = checked_doms.length; i < max_i; i++) {
+                m_true_values.push(checked_doms.eq(i).val());
+            }
+            select_jQDom.val(m_true_values).trigger("change"); //賦值，且可觸發原來  select 的事件
+        });
+    }.bind(reqk, select_jQDom);
+    //如果使用者仍對 select 作 value 操作，也可以改變 checkbox 的值
+    select_jQDom.data()["selectToCheckbox_event"] = function (selfDom, reqk) {
+        var val = selfDom.val();
+        //console.log(val);
+        //clean all
+        if (val != null) {
+            $("input[type='checkbox'][reqk='" + reqk + "']").prop('checked', false);
+            for (var i = 0, max_i = val.length; i < max_i; i++) {
+                $("input[type='checkbox'][reqk='" + reqk + "']").filter("[value='" + val[i] + "']").prop('checked', true);
+            }
+        }
+    };
+    select_jQDom.bind("change", {
+        "selfDom": select_jQDom,
+        "reqk": reqk
+    }, function (e) {
+        e.data.selfDom.data()["selectToCheckbox_event"](e.data.selfDom, e.data.reqk);
+    });
+    //產生 checkbox 內容
+    var m_checkbox_html = new Array();
+    var select_jQDom_nowValue = select_jQDom.val();
+    m.map(function (item) {
+        var strIsChecked = "";
+        if (select_jQDom_nowValue.includes(item.value)) {
+            strIsChecked = " checked ";
+        }
+        var d = `<label><input type="checkbox" reqk="${reqk}" value="${item.value}" name="${reqk}" ${strIsChecked}>${item.value_label}</label>`;
+        m_checkbox_html.push(d);
+    });
+    output['html'] = "<span reqk='" + reqk + "' ";
+    if (default_options['class'] != null) {
+        output['html'] += ` class="${default_options['class']}"`;
+    }
+    output['html'] += ">";
+    output['html'] += m_checkbox_html.join(default_options.separate) + "</span>";
+    if (default_options.output_format == "dom") {
+        if (default_options.checkboxhtml_to_dom != null) {
+            //使用者有指定要放哪，直接蓋掉
+            default_options.checkboxhtml_to_dom.html(output['html']);
+        } else {
+            //如果是 null，就 after 到原來的 select
+            select_jQDom.after(output['html']);
+        }
+        output['jQDom'] = $("input[reqk='" + reqk + "']");
+    }
+    //隱藏原來的 select
+    $(select_jQDom).hide();
+    //執行事件
+    output['execute_event']();
+    //輸出結果
+    /*
+      output['jQDom'] checkbox 的 jquery 物件們(陣列)
+      output['html'] checkbox 最後集合的 html 文字內容;
+      output['reqk'] checkbox 裡 reqk 的臨時編號
+      output['execute_event'] 如果使用 html 回傳，那就要自行執行此 function 才有 select 、checkbox 雙向事件綁定
+    */
+    return output;
+}
