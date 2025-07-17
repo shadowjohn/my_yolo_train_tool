627a4fdd1e7d38b9cd96b7e0e2e19f9758db37fa
John <john@gis.tw>
2025-07-17 18:30:28 +0800

    研究 tensorflow 版本相依問題研究 tensorflow 問題

--- build_conda_env.bat
+++ build_conda_env.bat
@@ -7,7 +7,7 @@ rem 4. ¦w¸Ë yolo
 SET mypath=%~dp0
 echo %mypath%
 mkdir binary
-call conda create --prefix "%mypath%\binary\Ultralytics" python=3.9 -y
+call conda create --prefix "%mypath%\binary\Ultralytics" python=3.10 -y
 call conda activate "%mypath%\binary\Ultralytics"
 cd /d "%mypath%\binary\Ultralytics"
 rem call scripts\pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
@@ -15,5 +15,8 @@ rem call scripts\pip install -r ..\..\requirements.txt
 rem ultralytics ?
 call conda install -y pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 call scripts\pip install ultralytics pywin32 keyboard flask
+rem call scripts\pip install pywin32 keyboard flask ultralytics==8.0.186 torch==2.0.1 torchvision==0.15.2 tensorflow==2.13.0 onnx==1.13.1 onnx2tf==1.22.0 opencv-python==4.8.0.76 h5py==3.10.0 flatbuffers==24.3.25
+rem call conda install -y tensorflow
+call scripts\pip install tensorflow-cpu==2.10.0
 
 
