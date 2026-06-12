42db0f089011394e2b5701c1c35db94cca673982
John <john@gis.tw>
2026-06-12 18:10:18 +0800

    feat: add official Live2D model3 assets

---
+++ www/live2d/assets/live2d-runtime-shim.js
@@ -0,0 +1,8 @@
+(function () {
+  if (typeof Live2DCubismCore !== "undefined" && !window.Live2DCubismCore) {
+    window.Live2DCubismCore = Live2DCubismCore;
+  }
+  if (typeof PIXI !== "undefined" && !window.PIXI) {
+    window.PIXI = PIXI;
+  }
+})();
